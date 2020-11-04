a7383046bd9f4985ef87d4555d8912a849d59fbe
ynotdoan <73931638+ynotdoan@users.noreply.github.com>
2020-11-04 00:52:19 -0800

    Add files via upload

---
+++ Lab20_01/src/FirstProgram.java
@@ -0,0 +1,13 @@
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Hello, ");
+		System.out.println("World!!!");
+		System.out.println("My nam is Tony Doan.");
+		System.out.print("Welcome to CSE20 at UC Merced.");
+	}
+
+}
